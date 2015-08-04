1aa8b7ddb4c508bd684c6f86fbcef81fb598cdee
Nicholas Tau <fengping.tao@upai.com>
2015-08-04 12:00:51 +0800

    retry image custom supported.

--- Pod/Classes/MWPhoto.h
+++ Pod/Classes/MWPhoto.h
@@ -20,6 +20,7 @@
 @property (nonatomic, strong) NSURL *videoURL;
 @property (nonatomic) BOOL emptyImage;
 @property (nonatomic) BOOL isVideo;
+@private  (nonatomic, strong) UIImage * retryImage;
 
 + (MWPhoto *)photoWithImage:(UIImage *)image;
 + (MWPhoto *)photoWithURL:(NSURL *)url;
